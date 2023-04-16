826e31b770efd2a812f0714730a064afd3fb31b4
Racquel Glickman <racquel.glickman@gmail.com>
2023-04-16 19:20:51 -0400

    completes use effect lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    console.log('useEffect running')
+
+      const timer = setTimeout(() => setTimeRemaining(timeRemaining - 1), 1000);
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+    }
+
+    return  () => {
+      clearTimeout(timer)
+    }
+
+  }, [timeRemaining]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
